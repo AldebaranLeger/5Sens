1dcd75e13d930caffca2fa18840503d8be7d79a3
Rémi Caillot <remicaillot5@gmail.com>
2019-03-28 20:01:15 +0100

    proposition

---
+++ src/components/home.vue
@@ -0,0 +1,116 @@
+<template>
+    <main>
+        <div class="login-card ">
+            <h1><img src="../assets/images/monstre_login.PNG" alt="" height="" width="">
+            </h1><br>
+            <form
+            @submit.prevent="onSubmit">
+                <input type="text" name="user" placeholder="Email" v-model="email">
+                <input type="password" name="pass" placeholder="Mot de passe" v-model="password">
+                <input type="submit" name="login" class="login login-submit" value="se connecter">
+            </form>
+
+            <div class="login-help">
+                <a href="#">Créer un compte</a> • <a href="#">Mot de passe oublié</a>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+    export default {
+        name: "home",
+        data() {
+            return {
+                email: "",
+                password: ""
+            }
+        },
+        methods: {
+            onSubmit(e) {
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    body {
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu,
+        Cantarell, 'Helvetica Neue', Helvetica, Arial, sans-serif;
+    }
+
+    .login-card {
+        padding: 40px;
+        background-color: #a1dddf;
+        margin: 0 auto 10px;
+        box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+        overflow: hidden;
+    }
+
+    .login-card h1 {
+        text-align: center;
+        font-size: 2.3em;
+    }
+
+    .login-card input[type=submit] {
+        width: 100%;
+        display: block;
+        margin-bottom: 10px;
+        position: relative;
+    }
+
+    /* Textes à l'intérieur des Input */
+    .login-card input[type=text],
+    input[type=password] {
+        font-weight: bold;
+        color: white;
+        height: 44px;
+        font-size: 16px;
+        width: 100%;
+        margin-bottom: 20px;
+        background: #a1dddf;
+        border: none;
+        border-bottom: 2px solid white;
+        padding: 0 8px;
+        box-sizing: border-box;
+        text-align: center;
+    }
+
+    .login {
+        border: none;
+        text-align: center;
+        font-size: 14px;
+        font-family: 'Arial', sans-serif;
+        height: 36px;
+        padding: 0 8px;
+    }
+
+    .login-submit {
+        font-weight: bold;
+        margin-top: 20px;
+        margin-bottom: 10px;
+        border: 2px solid white;
+        border-radius: 30px;
+        color: #fff;
+        text-shadow: 0 1px rgba(0, 0, 0, 0.1);
+        background-color: #a1dddf;
+    }
+
+    /* Les liens vers mots de passe oublié et créer un compte */
+    .login-card a {
+        text-decoration: none;
+        color: #666;
+        text-align: center;
+        display: inline-block;
+        opacity: 0.6;
+        transition: opacity ease 0.5s;
+    }
+
+    /* le "footer" de la carte de login */
+    .login-help {
+        width: 100%;
+        text-align: center;
+        font-size: 12px;
+    }
+
+</style>
