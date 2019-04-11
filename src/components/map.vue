f8e7022bd68b13760c72ab7f07a84f351d18b036
StarRunner <aldebaran.leger@gmail.com>
2019-04-11 16:39:58 +0200

    Ajout de la page Map.vue, la map ne s'affiche pas, cependant aucunes erreur n'est lancée. Je pense que la map ne charge pas entièrement.

--- src/components/map.vue
+++ src/components/map.vue
@@ -1,38 +1,42 @@
 <template>
-    <MglMap
-            :accessToken="accessToken"
-            :mapStyle.sync="mapStyle"
-            @load="onMapLoaded"
-    />
+
+        <MglMap :accessToken="accessToken" :mapStyle.sync="mapStyle" />
+        <mgl-map><MglNavigationControl position="top-right"/>
+                <MglGeolocateControl position="top-right" />
+        </mgl-map>
+
 </template>
 
 <script>
 
-    import Mapbox from "mapbox-gl";
-    import {MglMap} from "vue-mapbox";
+        import Mapbox from "mapbox-gl";
+        import { MglMap, MglNavigationControl, MglGeolocateControl } from "vue-mapbox";
 
-    export default {
-        name: "map",
-        components: {
-            MglMap
-        },
-        data() {
-            return {
-                accessToken: "pk.eyJ1Ijoia2FpemUiLCJhIjoiY2ptdnB5cTFnMmN2bzNxcG40azdhZXZuYiJ9.aSLGuoI6UckRciUJj2MqRg", // your access token. Needed if you using Mapbox maps
-                mapStyle: "mapbox://styles/kaize/cjuce30n91as21fjxd2oaxeq4" // your map style
-            };
-        },
-        methods: {
-            onMapLoaded(e) {
-                const map = e.map;
-            }
-        },
-    };
+
+        export default {
+                components: {
+                        MglMap,
+                        MglNavigationControl,
+                        MglGeolocateControl
+                },
+                data() {
+                        return {
+                                accessToken: "pk.eyJ1Ijoia2FpemUiLCJhIjoiY2ptdnB5cTFnMmN2bzNxcG40azdhZXZuYiJ9.aSLGuoI6UckRciUJj2MqRg",
+                                mapStyle: "mapbox://styles/kaize/cjuco34fx3q3q1fnm9oatomzu"
+                        };
+                },
+
+
+        };
 
 </script>
 
 
 <style scoped>
 
+    body {
+        margin:0;
+        padding:0;
+    }
 
 </style>
