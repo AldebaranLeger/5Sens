5a18a4002bf4c92db972fcb72e30e35e4947947a
StarRunner <aldebaran.leger@gmail.com>
2019-05-03 10:40:09 +0200

    La carte est fonctionnelle

--- src/components/map.vue
+++ src/components/map.vue
@@ -1,33 +1,34 @@
 <template>
 
-        <MglMap :accessToken="accessToken" :mapStyle.sync="mapStyle" />
-        <mgl-map><MglNavigationControl position="top-right"/>
-                <MglGeolocateControl position="top-right" />
-        </mgl-map>
+    <div id="map">
+        <mapbox
+                :access-token="accessToken"
+                :fullscreen-control="{show: true,position: 'top-left'}"
+                :geolocate-control="{show: true,position: 'top-left'}"
+                :map-options="{style: mapStyle}"
+                :scale-control="{show: true,position: 'top-left'}">
+        </mapbox>
+    </div>
 
 </template>
 
 <script>
-
-        import Mapbox from "mapbox-gl";
-        import { MglMap, MglNavigationControl, MglGeolocateControl } from "vue-mapbox";
+    import Mapbox from 'mapbox-gl-vue';
 
 
-        export default {
-                components: {
-                        MglMap,
-                        MglNavigationControl,
-                        MglGeolocateControl
-                },
-                data() {
-                        return {
-                                accessToken: "pk.eyJ1Ijoia2FpemUiLCJhIjoiY2ptdnB5cTFnMmN2bzNxcG40azdhZXZuYiJ9.aSLGuoI6UckRciUJj2MqRg",
-                                mapStyle: "mapbox://styles/kaize/cjuco34fx3q3q1fnm9oatomzu"
-                        };
-                },
+    export default {
+        components: {
+            Mapbox,
+        },
+        data() {
+            return {
+                accessToken: "pk.eyJ1Ijoia2FpemUiLCJhIjoiY2ptdnB5cTFnMmN2bzNxcG40azdhZXZuYiJ9.aSLGuoI6UckRciUJj2MqRg",
+                mapStyle: "mapbox://styles/kaize/cjuco34fx3q3q1fnm9oatomzu"
+            };
+        },
 
 
-        };
+    };
 
 </script>
 
@@ -35,8 +36,14 @@
 <style scoped>
 
     body {
-        margin:0;
-        padding:0;
+        margin: 0;
+        padding: 0;
     }
+
+    #map {
+        width: 100%;
+        height: 500px;
+    }
+
 
 </style>
--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import '@babel/polyfill';
 import App from "./components/app.vue";
 import VueRouter from "vue-router";
 import router from './router';
+import Mapbox from 'mapbox-gl-vue';
+
 
 Vue.use(VueRouter);
 
 new Vue({
     router,
+    components: {
+        'mapbox': Mapbox
+    },
     render: createElement => createElement(App)
 }).$mount('#app');
