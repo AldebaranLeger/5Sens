d14a0c906eeeef59b44bd36bdd6070d61418e626
StrowBeary <remicaillot5@gmail.com>
2019-05-15 17:32:16 +0200

    le carroussel

---
+++ src/TimeKit.js
@@ -0,0 +1,96 @@
+export function Animate(from_value, to_value, easing_function, step) {
+    let increment = 0;
+    let is_running = false;
+    let is_ended = false;
+    let internal_is_ended = false;
+    return {
+        get is_running() {
+            return is_running
+        },
+        get is_ended_signal() {
+            const old =  is_ended;
+            is_ended = false;
+            return old;
+        },
+        start() {
+            is_running = true;
+            is_ended = false;
+            internal_is_ended = false;
+            increment = 0;
+        },
+        tick() {
+            if(is_running) {
+                if(increment <= 1) {
+                    const progress = easing_function(increment);
+                    const new_value = (1 - progress) * from_value + progress * to_value;
+                    increment += step;
+                    return new_value;
+                } else {
+                    is_running = false;
+                    is_ended = true;
+                    internal_is_ended = true;
+                }
+            }
+            return internal_is_ended ? to_value : from_value;
+        }
+    }
+}
+
+
+export const Easing = {
+    // no easing, no acceleration
+    linear: function (t) {
+        return t
+    },
+    // accelerating from zero velocity
+    easeInQuad: function (t) {
+        return t * t
+    },
+    // decelerating to zero velocity
+    easeOutQuad: function (t) {
+        return t * (2 - t)
+    },
+    // acceleration until halfway, then deceleration
+    easeInOutQuad: function (t) {
+        return t < .5 ? 2 * t * t : -1 + (4 - 2 * t) * t
+    },
+    // accelerating from zero velocity
+    easeInCubic: function (t) {
+        return t * t * t
+    },
+    // decelerating to zero velocity
+    easeOutCubic: function (t) {
+        return (--t) * t * t + 1
+    },
+    // acceleration until halfway, then deceleration
+    easeInOutCubic: function (t) {
+        return t < .5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1
+    },
+    // accelerating from zero velocity
+    easeInQuart: function (t) {
+        return t * t * t * t
+    },
+    // decelerating to zero velocity
+    easeOutQuart: function (t) {
+        return 1 - (--t) * t * t * t
+    },
+    // acceleration until halfway, then deceleration
+    easeInOutQuart: function (t) {
+        return t < .5 ? 8 * t * t * t * t : 1 - 8 * (--t) * t * t * t
+    },
+    // accelerating from zero velocity
+    easeInQuint: function (t) {
+        return t * t * t * t * t
+    },
+    // decelerating to zero velocity
+    easeOutQuint: function (t) {
+        return 1 + (--t) * t * t * t * t
+    },
+    // acceleration until halfway, then deceleration
+    easeInOutQuint: function (t) {
+        return t < .5 ? 16 * t * t * t * t * t : 1 + 16 * (--t) * t * t * t * t
+    },
+    bounceOut(t) {
+        return t * Math.abs(Math.sin(t * Math.PI * 2.5) + t)
+    }
+};
